71941c9d1d5b69571f31dd4109e7efe1e0b59156
Yaroslav.S <sysoev.yar@yandex.ru>
2022-06-18 16:25:45 +0500

    Решил 2 задачу

---
+++ HW2_elementaryMultipliers.py
@@ -0,0 +1,19 @@
+# Задайте натуральное число N. Напишите программу, которая составит
+# список простых множителей числа N.
+
+N = int(input("Введи число N = "))
+
+def elementary_mult(n):
+   i = 2
+   primfac = []
+   while i * i <= n:
+       while n % i == 0:
+           primfac.append(i)
+           n = n / i
+       i = i + 1
+   if n > 1:
+       primfac.append(n)
+   return primfac
+
+
+print(elementary_mult(N))
